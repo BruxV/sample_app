a00192107ab499c35fa087fc65af8de36eac0c7d
Brux Voight <bruxvoight@sbcglobal.net>
2012-02-15 17:49:38 -0600

    Added help page, done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+      @base_title = "Ruby on Rails Sample Tutorial App"
+  end
+      
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,9 +15,8 @@ describe PagesController do
     
     it 'should have the right title' do
       get 'home'
-      response.should have_selector('title', 
-                        :content => 
-                          'Ruby on Rails Sample Tutorial App | Home')
+      response.should have_selector("title", 
+                        :content => @base_title + ' | Home')
     end
   end
 
@@ -25,9 +28,8 @@ describe PagesController do
     
     it 'should have the right title' do
       get 'contact'
-      response.should have_selector('title',
-                        :content =>
-                          'Ruby on Rails Sample Tutorial App | Contact')
+      response.should have_selector("title",
+                        :content => @base_title + ' | Contact')
     end
   end
   
@@ -39,9 +41,22 @@ describe PagesController do
     
     it 'should have the right title' do
       get 'about'
-      response.should have_selector('title',
-                        :content =>
-                          'Ruby on Rails Sample Tutorial App | About')
+      response.should have_selector("title",
+                        :content => @base_title + ' | About')                        
     end
   end
+  
+  describe "GET 'help" do
+    it 'should be successful' do
+      get 'help'
+      response.should be_success
+    end
+    
+    it 'should have the right title' do
+      get 'help'
+      response.should have_selector("title",
+                        :content => @base_title + ' | Help')
+    end                    
+  end
+  
 end
